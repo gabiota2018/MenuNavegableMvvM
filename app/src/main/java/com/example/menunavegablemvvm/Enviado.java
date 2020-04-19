c86d7a34515908bc61acea2390511cc59c57954b
gabiota2018 <gabisago6@gmail.com>
2020-04-19 02:42:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/menunavegablemvvm/Enviado.java
@@ -0,0 +1,19 @@
+package com.example.menunavegablemvvm;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Enviado extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_enviar, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/menunavegablemvvm/ui/slideshow/SlideshowFragment.java
@@ -0,0 +1,37 @@
+package com.example.menunavegablemvvm.ui.slideshow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
+
+import com.example.menunavegablemvvm.R;
+
+public class SlideshowFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+        Button enviar=root.findViewById(R.id.enviar);
+        enviar.setOnContextClickListener(new View.OnContextClickListener() {
+            @Override
+            public boolean onContextClick(View view) {
+                Navigation.findNavController(getActivity(),R.id.nav_host_fragment).navigate(R.id.enviar);
+                return false;
+            }
+        });
+        return root;
+    }
+}
